86f07d3844c0ea14a1ba6a322e69ada1d9423603
JoramWilander <jwawilander@gmail.com>
2019-02-08 13:50:42 -0500

    Add starting point

---
+++ server/manifest.go
@@ -0,0 +1,9 @@
+package main
+
+var manifest = struct {
+	Id      string
+	Version string
+}{
+	Id:      "com.mattermost.custom-attributes",
+	Version: "0.0.1",
+}
---
+++ webapp/src/manifest.js
@@ -0,0 +1,2 @@
+export const id = 'com.mattermost.custom-attributes';
+export const version = '0.0.1';
