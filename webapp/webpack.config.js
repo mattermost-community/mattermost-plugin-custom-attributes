c2bc39b4570ae59ba05da522cf43a3ee37028358
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2022-10-03 23:40:56 -0400

    add react-dom to webpack externals (#81)

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -50,6 +50,7 @@ module.exports = {
     },
     externals: {
         react: 'React',
+        'react-dom': 'ReactDOM',
         redux: 'Redux',
         'react-redux': 'ReactRedux',
         'prop-types': 'PropTypes',
