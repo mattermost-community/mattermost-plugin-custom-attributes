ad2d9f93f6e0033a3c6ae5c11671e4281c07b2a3
nang2049 <a-nevy.angelova@mattermost.com>
2026-09-25 15:44:16 +0700

    Align React dependencies with the Mattermost v12 host
    Declare react/react-dom 19.2.8, react-redux 9.2.0, redux 5.0.1,
    @types/react 19 and prop-types so types, lint and tests match the host
    runtime. Remove Enzyme, which does not support React 19; no tests used
    it beyond the adapter setup.

--- webapp/src/tests/setup.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({adapter: new Adapter()});
